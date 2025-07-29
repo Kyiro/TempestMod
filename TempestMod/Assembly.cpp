3da3a16aba565445c8c74d6bc235c07238c4c7db
Kyiro <54126666+Kyiro@users.noreply.github.com>
2025-07-29 14:34:37 +0200

    update for ob57 and add a bot crash fix

--- TempestMod/Assembly.cpp
+++ TempestMod/Assembly.cpp
@@ -1,15 +1,15 @@
 #include "Assembly.h"
-#include "Memory.h"
 #include "Platform.h"
 #include <Windows.h>
 #include <detours/detours.h>
 
 inline int(THISCALL* oUnknownFunc)(void* This, void* a2);
+inline int(THISCALL* oBotFunc)(void* This, int a2);
 inline int(THISCALL* AssemblyLoad)(void* This, char a2);
 inline void* AssemblyGlobal;
 inline bool bLoadedAssembly;
 
-int __fastcall hUnknownFunc(THIS(void*, This), void* a2)
+int __fastcall hUnknownFunc(THIS(void* This), void* a2)
 {
 	if (!bLoadedAssembly && AssemblyLoad(AssemblyGlobal, 1))
 	{
@@ -19,18 +19,26 @@ int __fastcall hUnknownFunc(THIS(void*, This), void* a2)
 	return oUnknownFunc(This, a2);
 }
 
+int __fastcall hBotFunc(THIS(void* This), int a2)
+{
+	return 1;
+}
+
 void Assembly::InitHook()
 {
-	auto ModuleBase = Memory::GetModuleBase();
+	auto ModuleBase = GetModuleHandle(nullptr);
 
-	oUnknownFunc = reinterpret_cast<decltype(oUnknownFunc)>(ModuleBase + 0x63EB0);
-	AssemblyLoad = reinterpret_cast<decltype(AssemblyLoad)>(ModuleBase + 0xC46F60);
-	AssemblyGlobal = reinterpret_cast<decltype(AssemblyGlobal)>(ModuleBase + 0x1EF9970);
+	oBotFunc = reinterpret_cast<decltype(oBotFunc)>(ModuleBase + 0xBB3530);
+
+	oUnknownFunc = reinterpret_cast<decltype(oUnknownFunc)>(ModuleBase + 0x1924C);
+	AssemblyLoad = reinterpret_cast<decltype(AssemblyLoad)>(ModuleBase + 0x31CCB4);
+	AssemblyGlobal = reinterpret_cast<decltype(AssemblyGlobal)>(ModuleBase + 0x86E3E6);
 
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread()); 
 
 	DetourAttach(&(PVOID&)oUnknownFunc, hUnknownFunc);
+	DetourAttach(&(PVOID&)oBotFunc, hBotFunc);
 
 	DetourTransactionCommit();
 }
